8e5e99259340544f27420ab05fb00d6fb12f087c
chinghinguyen <nchinghi.yi9@gmail.com>
2025-02-26 09:48:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function login()
   let a = username.value
   let b = password.value
   if (a == "chinghi" && b == "369")
-  {window.open("home.html")}
+  {
+    window.open("home.html")
+  }
   else document.write("Không hợp lệ")
 }
