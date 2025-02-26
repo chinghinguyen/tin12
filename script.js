a5cc188624f24b87efcdd540f77c27148b68f9a9
chinghinguyen <nchinghi.yi9@gmail.com>
2025-02-26 09:31:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 { let a=username.value
   let b=password.value
   if (a=="chinghi" && b="123456789")
-  { window.open("home.html")
+  { window.open(home.html)
     document.getElementById(compiler).style.display="blocked"
   }
   else document.write("Không hợp lệ")
