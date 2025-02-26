410abac22335ae18b4c1fd6623271d0c92c3e1a5
chinghinguyen <nchinghi.yi9@gmail.com>
2025-02-26 09:49:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function login()
   let b = password.value
   if (a == "ncn" && b == "369")
   {
-    window.open("home.html")
+    document.open("home.html")
   }
   else document.write("Không hợp lệ")
 }
